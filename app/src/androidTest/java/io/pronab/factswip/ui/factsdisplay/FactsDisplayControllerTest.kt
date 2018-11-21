c0d017c6e40cef7443e357de90f62582831f843b
Pronab Pal <palpronab@gmail.com>
2018-11-22 04:33:50 +1100

    add Espresso tests for success and error

--- app/src/androidTest/java/io/pronab/factswip/ui/factsdisplay/FactsDisplayControllerTest.kt
+++ app/src/androidTest/java/io/pronab/factswip/ui/factsdisplay/FactsDisplayControllerTest.kt
@@ -1,37 +1,23 @@
 package io.pronab.factswip.ui.factsdisplay
 
 
-import android.support.test.espresso.DataInteraction
-import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-
-import android.support.test.InstrumentationRegistry.getInstrumentation
-import android.support.test.espresso.Espresso.onData
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.Espresso.pressBack
-import android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import android.support.test.espresso.action.ViewActions.*
-import android.support.test.espresso.assertion.ViewAssertions.*
-import android.support.test.espresso.matcher.ViewMatchers.*
-
 import io.pronab.factswip.R
-
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.anything
-import org.hamcrest.Matchers.`is`
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -44,19 +30,27 @@ class FactsDisplayControllerTest {
     @Test
     fun factsDisplayControllerTest() {
         val textView = onView(
-allOf(withText("About Canada"),
-childAtPosition(
-allOf(withId(R.id.facts_toolbar),
-childAtPosition(
-IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
-0)),
-0),
-isDisplayed()))
+            allOf(
+                withText("About Canada"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.facts_toolbar),
+                        childAtPosition(
+                            IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                            0
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
         textView.check(matches(withText("About Canada")))
-        }
-    
+    }
+
     private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
 
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
@@ -71,4 +65,4 @@ isDisplayed()))
             }
         }
     }
-    }
+}
